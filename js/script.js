07e61a0597ffe164d37d4ab8a7da85fa34c72b40
Eduardo Fuentes <ramieduar@gmail.com>
2022-06-22 21:57:48 -0600

    Se creo un nuevo archivo javascript para practicas Git

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var a = 10;
+var b = 10;
+
+function sumar()
+{
+    let resultado = this.a + this.b;
+    console.log('El resultado de la suma entre ' + this.a + ' + ' + this.b + ' es: ' + resultado);
+}
+
+sumar();
